e4bd7bd22172860a4cd33e1d101b248cc0275bef
Abhay20222 <112698889+Abhay20222@users.noreply.github.com>
2022-12-15 18:03:20 +0530

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo chmod -R 777 /home/ubuntu/node-express-realworld-example-app
+sudo chmod -R 777 /home/ubuntu/abhay-practice-node-express-realworld-example-app
 cd /home/ubuntu/abhay-practice-node-express-realworld-example-app
 pm2 start app.js
